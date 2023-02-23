33a43b0dabe4006f9594c4f90257f2224e7ee430
kyva0621 <tyrtyrforit@gmail.com>
2023-02-23 17:30:57 +0200

    Homework 7 (update)

--- java/homeworks/VKS_07/BookManager.java
+++ java/homeworks/VKS_07/BookManager.java
@@ -5,49 +5,41 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookManager {
-
-    private File findBook(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the title of the book in camelCase format");
-        String bookTitle = scanner.nextLine();
-
-        File file = new File("java\\homeworks\\VKS_07\\books\\" + bookTitle);
-
-        if(!file.exists()){
-            System.out.println("Book not found, please try again");
-            file = findBook();
-        }
-
-        return file;
-    }
+    private final String CLEAN_UP = "[.,\"'-?:!;]";
 
     public void getBookStatistic(){
+        System.out.println("Please enter the title of the book in camelCase format or 'exit' to exit");
         StringBuilder bookBuild = new StringBuilder();
-        File book = findBook();
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(book))) {
-            while(bufferedReader.ready()){
-                bookBuild.append(bufferedReader.readLine()).append(" ");
+        Scanner getTitle = new Scanner(System.in);
+        String title;
+        while(!(title = getTitle.nextLine()).equalsIgnoreCase("exit")){
+            File book = new File("java\\homeworks\\VKS_07\\books\\" + title);
+            try(BufferedReader bookBody = new BufferedReader(new FileReader(book))) {
+                while(bookBody.ready()){
+                    bookBuild.append(bookBody.readLine()).append(" ");
+                }
+            } catch (IOException e) {
+                System.out.println("Can't find book, please try again");
+                continue;
             }
-        } catch (IOException e) {
-            System.out.println(Arrays.toString(e.getStackTrace()));
-            return;
+            String withoutSpecialChar = bookBuild.toString().replaceAll(CLEAN_UP, "").toLowerCase();
+
+            int uniqueWords = Arrays.stream(withoutSpecialChar.split(" ")).collect(Collectors.toSet()).size();
+
+            Map<String, Integer> mostPopularWord = tenMostPopularWord(withoutSpecialChar);
+
+            String bookTitle = Arrays.stream(book.getName().split("\\.")).toList().get(0);
+
+            statisticLog(mostPopularWord, uniqueWords, bookTitle);
+            System.out.println("If you want get statistics about another book just print book title or 'exit' to exit");
         }
-        String withoutSpecialChar = bookBuild.toString().replaceAll("(?U)[^\\p{L}\\p{N}\\s]+(\\n)", "").toLowerCase();
-
-        int uniqueWords = Arrays.stream(withoutSpecialChar.split(" ")).collect(Collectors.toSet()).size();
-
-        Map<String, Integer> mostPopularWord = tenMostPopularWord(withoutSpecialChar);
-
-        String bookTitle = Arrays.stream(book.getName().split("\\.")).toList().get(0);
-
-        statisticLog(mostPopularWord, uniqueWords, bookTitle);
     }
 
     private Map<String, Integer> tenMostPopularWord(String sentence){
-        List<String> separatedWords = Arrays.stream(sentence.split(" ")).toList();
+        List<String> words = Arrays.stream(sentence.split(" ")).toList();
         HashMap<String, Integer> popularWords = new HashMap<>();
 
-        separatedWords.stream().filter(word -> word.length() > 2).forEach(word -> {
+        words.stream().filter(word -> word.length() > 2).forEach(word -> {
             if(popularWords.containsKey(word)){
                 popularWords.put(word, popularWords.get(word)+1);
             }else{
@@ -64,13 +56,11 @@ public class BookManager {
 
     private void statisticLog(Map<String, Integer> mostPopularWord, int uniqueWord, String bookTitle){
         StringBuilder bookLog = new StringBuilder();
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("java\\homeworks\\VKS_07\\books\\" + bookTitle + "_statistic.txt"))){
-            mostPopularWord.forEach((key, value) -> {
-                    bookLog.append(key).append(" -> ").append(value).append("\n");
-            });
-            bookLog.append("Number of unique words - ").append(uniqueWord);
-            bufferedWriter.write(bookLog.toString());
-            bufferedWriter.flush();
+        mostPopularWord.forEach((key, value) -> bookLog.append(key).append(" -> ").append(value).append("\n"));
+        bookLog.append("Number of unique words - ").append(uniqueWord);
+        try (BufferedWriter statisticLog = new BufferedWriter(new FileWriter("java\\homeworks\\VKS_07\\books\\" + bookTitle + "_statistic.txt"))){
+            statisticLog.write(bookLog.toString());
+            statisticLog.flush();
         } catch (IOException e) {
             System.out.println(Arrays.toString(e.getStackTrace()));
             return;
